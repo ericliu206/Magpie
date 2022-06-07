f8849a8c99632484e5d6b19113c9c0e9342f7c00
Eric Liu <ericliu206@gmail.com>
2022-06-06 18:57:27 -0700

    adjust camera position and direction from console

--- src/input.cpp
+++ src/input.cpp
@@ -1,5 +1,8 @@
 #include "input.h"
 
+#include <iostream>
+#include <sstream>
+
 void Magpie::Input::HandleInput(float deltaTime) {
     currentTime = SDL_GetTicks();
 
@@ -27,6 +30,31 @@ void Magpie::Input::HandleInput(float deltaTime) {
         if (currentTime-lastEscapeTime >= 100) {
             if (SDL_GetRelativeMouseMode()) {
                 SDL_SetRelativeMouseMode(SDL_FALSE);
+                std::cout << "set camera position(p) or direction(d): ";
+                std::string option;
+                std::cin >> option;
+                std::string input;
+                if (option == "p") {
+                    std::cout << "x y z: ";
+                    std::cin.ignore(1,'\n');
+                    getline(std::cin, input);
+                    std::stringstream ss(input);
+                    float x, y, z;
+                    ss >> x;
+                    ss >> y;
+                    ss >> z;
+                    camera->pos = Magpie::Vec3(x, y, z);
+                } else if (option == "d") {
+                    std::cout << "yaw pitch: ";
+                    std::cin.ignore(1,'\n');
+                    getline(std::cin, input);
+                    std::stringstream ss(input);
+                    float y, p;
+                    ss >> y;
+                    ss >> p;
+                    camera->yaw = y;
+                    camera->pitch = p;
+                }
             } else {
                 SDL_SetRelativeMouseMode(SDL_TRUE);
             }
