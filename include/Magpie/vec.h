cd1648cfdb4bc72a2012780ae512ec140ffc384a
Eric Liu <ericliu206@gmail.com>
2022-06-29 20:03:29 -0700

    Vec4 class

--- include/Magpie/vec.h
+++ include/Magpie/vec.h
@@ -13,15 +13,35 @@ namespace Magpie {
             Vec3& operator-=(const Vec3& rhs);
     };
 
+    class Vec4 {
+        public:
+            Vec4();
+            Vec4(float x, float y, float z, float w);
+            Vec4(const Vec4& other);
+            float x;
+            float y;
+            float z;
+            float w;
+            Vec4& operator+=(const Vec4& rhs);
+            Vec4& operator-=(const Vec4& rhs);
+    };
+
     namespace Vector {
         float Magnitude(Vec3 v);
+        float Magnitude(Vec4 v);
         Vec3 Normalize(Vec3 v);
+        Vec4 Normalize(Vec4 v);
         Vec3 Cross(Vec3 a, Vec3 b);
     }
 }
 
 Magpie::Vec3 operator+(Magpie::Vec3 lhs, const Magpie::Vec3& rhs);
+Magpie::Vec4 operator+(Magpie::Vec4 lhs, const Magpie::Vec4& rhs);
 Magpie::Vec3 operator-(Magpie::Vec3 lhs, const Magpie::Vec3& rhs);
+Magpie::Vec4 operator-(Magpie::Vec4 lhs, const Magpie::Vec4& rhs);
 Magpie::Vec3 operator*(Magpie::Vec3 lhs, float rhs);
-Magpie::Vec3 operator/(Magpie::Vec3 lhs, float rhs);
+Magpie::Vec4 operator*(Magpie::Vec4 lhs, float rhs);
 Magpie::Vec3 operator*(float lhs, Magpie::Vec3 rhs);
+Magpie::Vec4 operator*(float lhs, Magpie::Vec4 rhs);
+Magpie::Vec3 operator/(Magpie::Vec3 lhs, float rhs);
+Magpie::Vec4 operator/(Magpie::Vec4 lhs, float rhs);
--- src/vec.cpp
+++ src/vec.cpp
@@ -4,6 +4,7 @@
 
 using namespace Magpie;
 
+// Vec3
 Vec3::Vec3() {}
 
 Vec3::Vec3(float x, float y, float z) {
@@ -69,4 +70,68 @@ Vec3 Vector::Cross(Vec3 a, Vec3 b) {
     product.y = a.z*b.x - a.x*b.z;
     product.z = a.x*b.y - a.y*b.x;
     return product;
+}
+
+// Vec4
+Vec4::Vec4() {}
+
+Vec4::Vec4(float x, float y, float z, float w) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->w = w;
+}
+
+Vec4::Vec4(const Vec4& other) {
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+    this->w = other.w;
+}
+
+Vec4& Vec4::operator+=(const Vec4& rhs) {
+    this->x+=rhs.x;
+    this->y+=rhs.y;
+    this->z+=rhs.z;
+    this->z+=rhs.w;
+    return *this;
+}
+
+Vec4& Vec4::operator-=(const Vec4& rhs) {
+    this->x-=rhs.x;
+    this->y-=rhs.y;
+    this->z-=rhs.z;
+    this->z-=rhs.w;
+    return *this;
+}
+
+Vec4 operator+(Vec4 lhs, const Vec4& rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+Vec4 operator-(Vec4 lhs, const Vec4& rhs) {
+    lhs -= rhs;
+    return lhs;
+}
+
+Vec4 operator*(Vec4 lhs, float rhs) {
+    return Vec4(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.w * rhs);
+}
+
+Vec4 operator/(Vec4 lhs, float rhs) {
+    return Vec4(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w * rhs);
+}
+
+Vec4 operator*(float lhs, Vec4 rhs) {
+    return Vec4(lhs * rhs.x, lhs * rhs.y, lhs * rhs.z, lhs * rhs.w);
+}
+
+float Vector::Magnitude(Vec4 v) {
+    return sqrt(v.x*v.x + v.y*v.y + v.z*v.z + v.w*v.w);
+}
+
+Vec4 Vector::Normalize(Vec4 v) {
+    float m = Vector::Magnitude(v);
+    return v/m;
 }
